0d98067c78f5739f263c11dcff7584debaa31fa3
hao.wang <hao.wang@solita.fi>
2024-06-23 15:38:26 +0200

    Add Github Workflow files and GCP sources files.

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "raw" {
   project = var.project
-  name = "${var.data-project}-raw"
+  name = "${var.project}-raw"
   force_destroy = false
   uniform_bucket_level_access = true
   location = var.region
